ca960989589d4031d479dbbe5388b9632e355f61
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-22 11:55:37 +0500

    updated

--- Statistical_learning_algos_sample/python/Linear_regression.py
+++ Statistical_learning_algos_sample/python/Linear_regression.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import r2_score
+import math
+
 
 x_train = df_train.as_matrix(['x'])
 y_train = df_train.as_matrix(['y'])
@@ -11,4 +13,8 @@ y_test = df_test.as_matrix(['y'])
 clf = LinearRegression(normalize=True)
 clf.fit(x_train,y_train)
 y_pred = clf.predict(x_test)
+
 print(r2_score(y_test,y_pred))
+# print('R sq: ',clf.score(x_train,y_train))
+
+print('Correlation: ', math.sqrt(clf.score(x_train,y_train)))
