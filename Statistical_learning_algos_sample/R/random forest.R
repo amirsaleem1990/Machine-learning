89112787cddd973f63a30c08ffbb025b1bc51a5d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-18 15:10:45 +0500

    updated

--- Statistical_learning_algos_sample/R/random forest.R
+++ Statistical_learning_algos_sample/R/random forest.R
@@ -2,3 +2,6 @@ train$target_var <- as.factor(train$target_var)
 test$target_var <- as.factor(test$target_var)
 model_forest <- randomForest(target_var ~ ., data = train, nodesize=25, ntree=200)
 pred_forst <- predict(model_forest, nwedata=test)
+
+# confusion matrix
+table(test$target_var, pred_forst)
