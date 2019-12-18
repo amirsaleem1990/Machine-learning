e4cbb83d259dbd94bc727e4744eb4341a10ccfd5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-18 12:25:27 +0500

    updated

--- Statistical_learning_algos_sample/R/GLM.R
+++ Statistical_learning_algos_sample/R/GLM.R
@@ -1,8 +1,11 @@
 glm_model = glm(target_var ~ ., data = df, family = binomial)
-p = predict(glm_model, type="response")
 
+train_prediction = predict(glm_model, type="response") 
+test_prediction  = predict(glm_model, newdata=test, type="response") 
 # let us check if model works or not
-tapply(p, df$target_var, mean)
+tapply(test_prediction, df$target_var, mean)
 # 0       1
 # 0.1	  0.44
 # This means the models predict on average grater values for those are actually 1
+
+
