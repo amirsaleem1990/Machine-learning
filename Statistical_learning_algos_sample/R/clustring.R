7be33d480852b9e43838af3c72dde48cccf45173
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-20 18:12:17 +0500

    updated

--- Statistical_learning_algos_sample/R/clustring.R
+++ Statistical_learning_algos_sample/R/clustring.R
@@ -1,3 +1,7 @@
 distance <- dist(df, method = "euclidean")
 cluster <- hclust(distance, method = "ward") # The <ward> method cares about the distance between clusters using centric distance and also variance(shayad yehi word bola) of each cluster 
 plot(clusters)
+
+
+# let us label each data point according to what cluster belong to.
+cluster.Groups <- cutree(clusters, k  =10) # since we want 10 clusters
