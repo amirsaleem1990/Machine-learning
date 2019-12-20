960d03dddad91f2e32fa9d2381e4598ff7dc4bdc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-20 13:03:46 +0500

    updated

--- Statistical_learning_algos_sample/R/NLP.R
+++ Statistical_learning_algos_sample/R/NLP.R
@@ -9,3 +9,15 @@ corpus = tm_map(corpus, removePunctuation)
 corpus = tm_map(corpus, removeWords, stopwords("english"))
 corpus = tm_map(corpus, stemDocument)
 
+
+# Create matrix
+dtm = DocumentTermMatrix(corpus)
+
+# Remove any <token> does not appear at least 3% documents
+dtm = removeSparseTerms(dtm, 0.97)
+
+# Create data frame
+labeledTerms = as.data.frame(as.matrix(dtm))
+
+# Add in the outcome variable
+labeledTerms$responsive = emails$responsive
