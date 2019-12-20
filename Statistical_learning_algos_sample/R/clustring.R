4c61089134f5a556087eb23c62428de877b1dc27
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-20 18:13:18 +0500

    updated

--- Statistical_learning_algos_sample/R/clustring.R
+++ Statistical_learning_algos_sample/R/clustring.R
@@ -5,3 +5,6 @@ plot(clusters)
 
 # let us label each data point according to what cluster belong to.
 cluster.Groups <- cutree(clusters, k  =10) # since we want 10 clusters
+
+# lst us figger out what clusters are like.
+tapply(df$target, cluster.Groups, mean)
