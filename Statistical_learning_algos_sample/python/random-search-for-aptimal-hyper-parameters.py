d9fa6c6f97c30a09374100a182f1079c1c607dab
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-07 12:32:11 +0500

    updated

--- Statistical_learning_algos_sample/python/random-search-for-aptimal-hyper-parameters.py
+++ Statistical_learning_algos_sample/python/random-search-for-aptimal-hyper-parameters.py
@@ -1,11 +1,11 @@
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.svm import SVC
 paramter = [{
-	"C" : [1, 10, 100, 1000],
-	"gamma" : [1, 0.1, 0.01, 0.001, 0.0001],
+	"C" : stats.uniform(2,10),
+	"gamma" : stats.uniform(0.1, 1),
 	"kernel" : ["rbf"]
 }]
 
-classifier = GridSearchCV(SVC(), paramter, cv=5, scoring="accuracy")
+classifier = RandomizedSearchCV(SVC(), paramter, cv=5, scoring="accuracy")
 classifier.fit(X_train, y_train)
 print(classifier.best_params_)
