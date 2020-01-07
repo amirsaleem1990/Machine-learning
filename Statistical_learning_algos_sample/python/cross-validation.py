3fe17d14e378e999c9b111dd5047fe1d30f50d0e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-07 11:22:37 +0500

    updated

--- Statistical_learning_algos_sample/python/cross-validation.py
+++ Statistical_learning_algos_sample/python/cross-validation.py
@@ -1,3 +1,10 @@
 from sklearn.model_selection import KFold
 kf = KFold(n_splits=5)
 kf.get_n_splits(X_train)
+
+
+# stratified version(keep distribution of target variable is equal in all KFOLDS)
+from sklearn.model_selection import StrafiedKFold
+skf = StrafiedKFold(n_splits=5)
+skf.get_n_splits(X_train, y_train)
+
