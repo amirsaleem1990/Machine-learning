22d0c44b269e4e1fcea9ba88672db0e570d0e6be
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-24 17:48:40 +0500

    updated

--- EDA-in-python/open_summary.sh
+++ EDA-in-python/open_summary.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 ipynb-py-convert EDA-by-column.py EDA-by-column.ipynb
+if [[ $? != 0 ]] ; then exit ; fi
 jupyter nbconvert --to notebook --execute  EDA-by-column.ipynb --output EDA-by-column.ipynb
+if [[ $? != 0 ]] ; then exit ; fi
 jupyter nbconvert --to html EDA-by-column.ipynb
+if [[ $? != 0 ]] ; then exit ; fi
 gopen EDA-by-column.html
 read -p "Press any key when you closed your Summary: "
 echo -e "\nRemoving files EDA-by-column.{ipynb,html}.......... "
