7aef57a211cded10f0f8023324ccb3e158359888
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-25 10:36:55 +0500

    updated

--- EDA-in-python/EDA-by-column.py
+++ EDA-in-python/EDA-by-column.py
@@ -18,21 +18,24 @@ def new_line():
 
 df = pickle.load(open("df.pkl", "rb"))
 
-if df[target_variable].nunique() == 2:
-    print("\n-------------------- This is Binary Classification problem --------------------\n")
-    print("''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
-else:
-    print("\n-------------------- This is Multiclass Classification problem --------------------\n")
-    print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.preprocessing import LabelEncoder
-    target_variable = "AREA_NAME_EN"
-    df[target_variable] = LabelEncoder().fit_transform(df[target_variable].astype(str))
-    train_X, test_X, train_y, test_y = train_test_split(df.drop(columns=target_variable), df[target_variable])
+# if df[target_variable].nunique() == 2:
+#     print("\n-------------------- This is Binary Classification problem --------------------\n")
+#     print("''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+# else:
+#     print("\n-------------------- This is Multiclass Classification problem --------------------\n")
+#     print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+target_variable = "STATUS_CODE"
+
+for col in df.columns:
+    df[col] = LabelEncoder().fit_transform(df[col].astype(str))
+
+train_X, test_X, train_y, test_y = train_test_split(df.drop(columns=target_variable), df[target_variable])
+
+clf=RandomForestClassifier(n_estimators=200).fit(train_X, train_y)
+(clf.predict(test_X) == test_y).mean()
 
 
-    clf=RandomForestClassifier(n_estimators=200).fit(train_X, train_y)
-
-help(RandomForestClassifier)
 
 # ================================================================================================================ END Modeling
