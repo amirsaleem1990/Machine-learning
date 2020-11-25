e9ebac3785f5b55a74eb62f184e4d7f7aeb2c979
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-25 14:21:11 +0500

    updated

--- EDA-in-python/ok.py
+++ EDA-in-python/ok.py
@@ -18,7 +18,7 @@ from statsmodels.regression.linear_model import OLS
 
 def new_line():
     print("-------------------------")
-    
+
 def RMSE(predictions):
     return round(np.sqrt(((test_y - predictions)**2).mean()))
 
@@ -570,6 +570,83 @@ if df[target_variable].dtype in [float, int]:
     new_line()
     print(f"Maximum correlation between Reseduals and any data columns is {f.values[0]}, with columns <{f.index[0]}>")
     # --------------------------------------------------------- END Random Forest
-# target_variable = 'AREA_NAME_EN'
-# Classififcation problem
 elif df[target_variable].dtype == "O":
+    # Classififcation problem
+    if df[target_variable].nunique() == 2:
+        print("\n-------------------- This is Binary Classification problem --------------------\n")
+        print("''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+        df = pd.concat([
+                        df.select_dtypes(exclude = "O"),
+                        pd.get_dummies(df.drop(columns=target_variable).select_dtypes("O")),
+                        df[[target_variable]]
+                        ], 1)
+
+        train_X, test_X, train_y, test_y = train_test_split(df.drop(columns=target_variable), df[target_variable])
+        clf = LogisticRegression().fit(train_X, train_y)
+        predictions = clf.predict_proba(test_X)
+        predictions = pd.Series(predictions[:, 0])
+        lst = []
+        for thresh in np.linspace(predictions.min(), predictions.max(), 50)[1:]:
+            pred = predictions < thresh
+
+            pred.loc[pred == True] = clf.classes_[0]
+            pred.loc[pred == False] = clf.classes_[1]
+
+            test_y = test_y.reset_index(drop=True)
+
+            TN = ((pred == clf.classes_[0]) & (test_y == clf.classes_[0])).sum()
+            TP = ((pred == clf.classes_[1]) & (test_y == clf.classes_[1])).sum()
+            FN = ((pred == clf.classes_[0]) & (test_y == clf.classes_[1])).sum()
+            FP = ((pred == clf.classes_[1]) & (test_y == clf.classes_[0])).sum()
+
+            p = TP / (TP + FP)
+            r = TP / (TP + FN)
+            f = 2 * ((p * r) / (p+r))
+
+            lst.append((thresh, (pred == test_y).mean(), p, r , f))
+
+        d = pd.DataFrame(lst, columns=["Thresold", "Accurecy(0-1)", "Precision", "Recall", "F1"])
+        d = d.set_index("Thresold")
+        d.plot(grid=True, figsize=(18,7));
+        plt.title("Model performance at diffrent Thresolds", size=18, color='red');
+        plt.xlabel("Thresold", size=14, color='red');
+        plt.ylabel("");
+        plt.show()
+    else:
+        print("\n-------------------- This is Multiclass Classification problem --------------------\n")
+        print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+
+        df.loc[:, df.select_dtypes("O").columns] = df.select_dtypes("O").apply(lambda x: pd.Series(LabelEncoder().fit_transform(x.astype(str))).astype(str))
+        train_X, test_X, train_y, test_y = train_test_split(df.drop(columns=target_variable), df[target_variable])
+        
+        clf=RandomForestClassifier(n_estimators=1000).fit(train_X, train_y)
+        predictions = clf.predict(test_X)
+        feature_imp = pd.Series(clf.feature_importances_,index=train_X.columns).sort_values(ascending=False)
+        if feature_imp.size > 30:
+            feature_imp = feature_imp.head(30)
+        feature_imp.plot(kind='barh', figsize=(17,10), grid=True);
+        plt.title("Feature importances Graph", size=18, color='red');
+        plt.xlabel("Importance", size=14, color='red');
+        plt.ylabel("Feature", size=14, color='red');
+        plt.show()
+        # ====
+        f = (test_y, predictions)
+        f_int = (test_y.astype(int), predictions.astype(int))
+        
+        print(f"accuracy_score: {metrics.accuracy_score(*f)}")
+        print(f"f1_score: {metrics.f1_score(*f_int)}")
+        
+        metrics.plot_roc_curve(clf, test_X, test_y);
+        plt.title("ROC curve plot");
+        plt.show();
+        
+        metrics.ConfusionMatrixDisplay(metrics.confusion_matrix(*f)); plt.show()
+        
+        metrics.plot_confusion_matrix(clf, test_X, test_y);
+        plt.title("Confusion matrix");
+        plt.show()
+        
+        metrics.plot_precision_recall_curve(clf, test_X, test_y);
+        plt.title("Precision recall curve");
+        plt.show()
+# ================================================================================================================ END Modeling
