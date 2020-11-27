a50a1f501dee25ef0b5cdf061dd057040bc1b4b4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-27 12:23:19 +0500

    updated

--- EDA-in-python/Modeling.R
+++ EDA-in-python/Modeling.R
@@ -3,18 +3,20 @@ suppressWarnings(library(dplyr))
 library(readr)
 df <- read.csv("df.csv")
 target_variable_name <- read_file("target_variable.txt")
-if is.numeric(df[[target_variable_name]]){
-
-    lm.full <- lm(as.formula(paste(target_variable_name, " ~ .")), data = df)
-    lm.null <- lm(as.formula(paste(target_variable_name, " ~ 1")), data = df)
-
-    model.aic.both <- step(
-      lm.full, direction = "both", scope = as.formula(paste("ACTUAL_WORTH ~ ", paste(names(train), collapse = " + ")))
-    )
-    save(model.aic.both, file="model.aic.both.rda")
-}
 
 smp_size <- floor(0.75 * nrow(df))
 train_ind <- sample(seq_len(nrow(df)), size = smp_size)
 train <- df[train_ind, ]
 test <- df[-train_ind, ]
+
+if  ( is.numeric( df[[target_variable_name]] ) ){
+
+    lm.full <- lm(as.formula(paste(target_variable_name, " ~ .")), data = train)
+    lm.null <- lm(as.formula(paste(target_variable_name, " ~ 1")), data = train)
+
+    model.aic.both <- step(
+      lm.full, direction = "both", scope = as.formula(paste(target_variable_name, "~ ", paste(names(train), collapse = " + ")))
+    )
+    cat("\nLinear regression model saved as <model_LM.aic.both.rda>\n")
+    save(model.aic.both, file="model_LM.aic.both.rda")
+}
