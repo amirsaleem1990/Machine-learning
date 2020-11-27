91fc0a98041f207932a87bc42054949b7349bc18
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-27 17:42:33 +0500

    updated

--- EDA-in-python/open_summary.sh
+++ EDA-in-python/open_summary.sh
@@ -12,26 +12,3 @@ DEL -rf EDA-by-column.{ipynb,html}
 if [[ $? == 0 ]] ; then
 	echo -e "\nFiles removed Successfully\n"
 fi
-
-
-
-
-
-
-
-
-
-
-./py-to-R.py
-if [[ $? != 0 ]] ; then exit ; fi
-jupyter nbconvert --to notebook --execute  Modeling.ipynb --output Modeling.ipynb
-if [[ $? != 0 ]] ; then exit ; fi
-jupyter nbconvert --to html Modeling.ipynb
-if [[ $? != 0 ]] ; then exit ; fi
-gopen Modeling.html
-read -p "Press any key when you closed your Summary: "
-echo -e "\nRemoving files Modeling.{ipynb,html}.......... "
-DEL -rf DEL -rf Modeling.{ipynb,html}
-if [[ $? == 0 ]] ; then
-	echo -e "\nFiles removed Successfully\n"
-fi
