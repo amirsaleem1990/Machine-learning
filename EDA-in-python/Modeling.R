d51b72dc29e44f24244dd7e50f79ba15c825940d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-11-27 12:15:32 +0500

    updated

--- EDA-in-python/Modeling.R
+++ EDA-in-python/Modeling.R
@@ -3,3 +3,15 @@ suppressWarnings(library(dplyr))
 library(readr)
 df <- read.csv("df.csv")
 target_variable_name <- read_file("target_variable.txt")
+if is.numeric(df[[target_variable_name]]){
+
+    lm.full <- lm(as.formula(paste(target_variable_name, " ~ .")), data = df)
+    lm.null <- lm(as.formula(paste(target_variable_name, " ~ 1")), data = df)
+
+    model.aic.both <- step(
+      lm.full, direction = "both", scope = as.formula(paste("ACTUAL_WORTH ~ ", paste(names(train), collapse = " + ")))
+    )
+    save(model.aic.both, file="model.aic.both.rda")
+}
+
+install.packages("scorecard")
