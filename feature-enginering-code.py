58e95e0dc282f1e5c14c21954995f0184e774595
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-01-27 17:36:30 +0500

    updated

--- feature-enginering-code.py
+++ feature-enginering-code.py
@@ -102,3 +102,13 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 yhat = model.predict(X_test)
 
+# make dummy classification data
+from sklearn.datasets import make_classification
+X, y = make_classification(n_samples=10000, 
+						   n_features=2, 
+						   n_redundant=0, # all features should be usefull for classification 
+						   n_clusters_per_class=1, 
+						   weights=[0.999], # imbalance  
+						   flip_y=0, 
+						   random_state=4)
+
